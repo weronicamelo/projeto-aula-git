04b615427d29c6a25ef4867534dfdb7c159e0114
weronicamelo <weronicaamelo@gmail.com>
2024-08-16 16:38:06 -0300

    Projeto Concluido

---
+++ Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace projeto_aula_git
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblaula = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblaula
+            // 
+            this.lblaula.AutoSize = true;
+            this.lblaula.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblaula.Location = new System.Drawing.Point(225, 191);
+            this.lblaula.Name = "lblaula";
+            this.lblaula.Size = new System.Drawing.Size(140, 20);
+            this.lblaula.TabIndex = 0;
+            this.lblaula.Text = "Aula de Git/Github";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(597, 450);
+            this.Controls.Add(this.lblaula);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblaula;
+    }
+}
+
